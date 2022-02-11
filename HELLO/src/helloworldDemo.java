a045a135bbf64f5dcccf26fb8990f5d6fe8e5c7b
shifhaaz <98401541+shifhaaz@users.noreply.github.com>
2022-02-11 23:24:36 +0000

    Update helloworldDemo.java

--- HELLO/src/helloworldDemo.java
+++ HELLO/src/helloworldDemo.java
@@ -3,7 +3,7 @@ public class helloworldDemo {
 	
 	public static void Main(String args[])
 	{
-		System.out.println("Hello World Program");
+		System.out.println("Hello World Program");//this is my first program yo!
 	}
 	 
 	
