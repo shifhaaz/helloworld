a3f1457d98fe60736fa775373c8352a46cffc125
sehar <sehar@Aishaaaa.Home>
2022-02-12 13:30:44 +0000

    Swaping of 2 nos.

---
+++ HELLO/src/Swaping.java
@@ -0,0 +1,19 @@
+
+public class Swaping {
+
+	public static void main(String[] args) {
+		float n1=2.5f;
+		float n2=-3.5f;
+		 System.out.println("Value before swap of n1\n"+n1);
+		 System.out.println("Value before swap of n2\n"+n2);
+		 
+		 float temp=n1;
+		 n1=n2;
+		 n2=temp;
+		 
+		 System.out.println("Value after swap of n1\n"+n1);
+		 System.out.println("Value after swap of n2\n"+n2);
+		 
+	}
+
+}
