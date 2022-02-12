80e15b6ba2322c74ef77db2e7ca3845f91f29c62
sehar <sehar@Aishaaaa.Home>
2022-02-12 12:49:30 +0000

    EvenAndOddProgram

---
+++ HELLO/src/EvenAndOdd.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class EvenAndOdd {
+	public static void main(String args[])
+	{
+	Scanner reader=new Scanner(System.in);
+	System.out.println("Enter a Number");
+	int num=reader.nextInt();
+	
+	if (num%2==0)
+	
+		System.out.println("this is even Number");
+
+		else
+			
+			System.out.println("this is odd number");
+	}
+	}
+	
+
+
