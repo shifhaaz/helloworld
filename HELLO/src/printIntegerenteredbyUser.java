548aae79cc423d9b187eaa297ed0f072d2a645d6
sehar <sehar@Aishaaaa.Home>
2022-02-12 12:01:49 +0000

    this is my program of taking  integer value from user

---
+++ HELLO/src/printIntegerenteredbyUser.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class printIntegerenteredbyUser {
+
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter a Number:");
+		int number = reader.nextInt();
+		System.out.println("You Entered:"+ number);
+		
+
+	}
+
+}
