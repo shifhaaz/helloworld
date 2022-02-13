d3299ea4583c78c21492b6070ea094ddcbef74c7
sehar <sehar@Aishaaaa.Home>
2022-02-13 00:29:08 +0000

    my escape sequence program

---
+++ HELLO/src/EscapeSequence.java
@@ -0,0 +1,10 @@
+
+public class EscapeSequence {
+
+	public static void main(String[] args) {
+		System.out.println("Hello \"Knight\" Batch");
+		System.out.println("Hello \tKnight\t Batch");
+		System.out.println("Hello \fKnight\f Batch");
+		}
+
+}
