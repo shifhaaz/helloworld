4adb76a44ccfb9ec5e679094b1e105230cdd4f10
sehar <sehar@Aishaaaa.Home>
2022-03-01 23:35:30 +0000

    static inner class

---
+++ HELLO/src/ch6/Innerclasses.java
@@ -0,0 +1,39 @@
+package ch6;
+
+public class Innerclasses {
+
+	public static void main(String[] args) {
+		outer.innernonstatic in = new outer().new innernonstatic();
+		in.met();
+			
+		outer.innerstatic ins = new outer.innerstatic();
+		ins.met();
+			
+
+	}
+
+}
+class outer{
+	void met() {
+		System.out.println("outer class non static method called....");
+		
+	}
+	static void met2() {
+		System.out.println("outer class static method called....");
+	}
+	 static class  innerstatic{
+		 public void met() {
+			 System.out.println("static method called....");	
+			 met2();
+		 }
+	 }
+	 class innernonstatic{
+		 public void met() {
+			 System.out.println("Non static method called....");	
+			 met2();
+			 met();
+			 
+		 }
+		 
+	 }
+}
--- HELLO/src/ch6/staticdemo.java
+++ HELLO/src/ch6/staticdemo.java
@@ -1,5 +1,9 @@
 package ch6;
 
+ class parent{
+	int i = 33;
+}
+
 public class staticdemo {
 	public staticdemo() {
 		System.out.println ("cons called....");
@@ -12,16 +16,19 @@ public static void main(String args[]) {
 	new staticdemo().met2(100);
 	staticdemo.met(10);
 	staticdemo.met22();
-	
+	int i = 3;
 }
 static void met(int i)
 {
-	System.out.println("static met is called");
+	System.out.println("static met is called"+i);
+	new staticdemo().i=200;
+	i = 40;
 }
 static void met22() {
 	System.out.println("static met22 is called");
 }
 void met2(int i) {
-	System.out.println(" Non static met is called");
+	System.out.println(" Non static met is called"+i);
+	System.out.println(super.i);
 }
 }
