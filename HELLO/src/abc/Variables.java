cd8d6a4869064141b12c6c2cfcd59857ac8f19ea
sehar <sehar@Aishaaaa.Home>
2022-04-19 13:13:41 +0100

    Practice session with zakiya

---
+++ HELLO/src/abc/Variables.java
@@ -0,0 +1,30 @@
+package abc;
+
+public class Variables {
+
+	public static void main(String[] args) {
+	String name="hello";//variable  is name of string type
+	//String is complex itself but behaves likes primitive type.
+	
+	 final int myNum= 23;
+	 Integer i= 45;
+	 
+	float myFloat= 2.3f;
+	Float ii=6.78f;
+	
+	double myDouble=4.67;
+	Double iiiD=4.76;
+	
+	char myChar='s';
+	Character iiiiC='r';
+	
+	boolean myboolean=true;
+	Boolean studentB=false;
+	
+	System.out.println(name);
+	System.out.println(myFloat+ "\n "+myDouble + "\n "+myChar );
+	System.out.println(i);
+	System.out.println(i+"\t"+ii+"\t"+iiiD+"\t"+iiiiC+"\t");
+	}
+
+}
